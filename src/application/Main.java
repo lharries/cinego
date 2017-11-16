041b0340b9c5b85c5d15793898eb583de7e3d376
Kai Klasen <ucabkpk@ucl.ac.uk>
2017-11-16 12:12:47 +0000

    initial drafts, ideas and folders

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,22 +1,38 @@
 package application;
 	
+import java.io.FileInputStream;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
-		try {
-			
+		try {	
 			//BorderPane root = new BorderPane();
 			
 			Parent root = FXMLLoader.load(getClass().getResource("/application/Login.fxml"));
 			Scene scene = new Scene(root,800,800);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			
+			
+			final ImageView backgroundImage = new ImageView();
+			Image image1 = new Image(new FileInputStream("/Users/kai/code/CineGo/cinego/src/Images/films.jpg"));
+			
+			backgroundImage.setImage(image1);
+			
+
+			//add image to background of Login.fxml
+			
+			
+			
+			
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {

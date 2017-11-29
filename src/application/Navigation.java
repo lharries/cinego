bf95d044ca23ac5fb2269ee7838a9edd7eae607c
Kai Klasen <ucabkpk@ucl.ac.uk>
2017-11-29 13:26:49 +0000

    adding overarching navigation class to cinema system

---
+++ src/application/Navigation.java
@@ -0,0 +1,28 @@
+package application;
+
+public class Navigation {
+
+    /**
+     * Constants locating the FXML files representing the different views
+     */
+    public static final String USER_ROOT = "/view/customerRoot.fxml";
+//    public static final String EMPL_ROOT = "/view/MgmtRootView.fxml";
+
+    public static final String TABLE_VIEW = "/view/EmpTableView.fxml";
+    public static final String EMP_ORDERS_VIEW = "/view/EmpOrdersView.fxml";
+    public static final String ORDER_VIEW = "/view/EmpOrderView.fxml";
+
+    public static final String MGMT_EMPLOYEES = "/view/MgmtEmployeesView.fxml";
+    public static final String MGMT_MENU = "/view/MgmtMenuView.fxml";
+    public static final String MGMT_ORDERS = "/view/MgmtOrdersView.fxml";
+
+    // Main controller for the specific rootViews
+//    private static EmployeeRootController empController;
+//    private static MgmtRootController mgmtController;
+
+
+
+
+
+
+}

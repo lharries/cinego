be8a02d2c9ad458b975a015c857e6f96a1b5adf4
Kai Klasen <ucabkpk@ucl.ac.uk>
2017-11-30 09:06:47 +0000

    writing Navigation and LoginController

--- src/application/CustomerProgramController.java
+++ src/application/CustomerProgramController.java
@@ -1,24 +1,34 @@
 package application;
 
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 /**
  * What functionalities does my customerProgramVIEW need to have and define these in this controller
  *
  */
-
-
 public class CustomerProgramController {
 
 
+   @FXML
+   private Button toCustProf;
 
-//EXAMPLE OF FUNCTIONALITY
-//    @FXML
 
 //    private void showProgramMovie(){
 //
 //    }
+//   @FXML
+//    public void goToCustAcc(ActionEvent event){
+//        Navigation.setCustController();
+//
+//    }
+
+
+
+
+
 
 
 }

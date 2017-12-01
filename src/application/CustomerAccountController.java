d1644848ffffcbb247a3c69c7c17b001968a780f
Kai Klasen <ucabkpk@ucl.ac.uk>
2017-12-01 14:35:57 +0000

    adding employee views

--- src/application/CustomerAccountController.java
+++ src/application/CustomerAccountController.java
@@ -1,4 +1,17 @@
 package application;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class CustomerAccountController {
+
+@FXML
+    private Button editProfile;
+
+
+
+private void editProfileClickHander() {
+    // TODO: Add in
+}
+
 }

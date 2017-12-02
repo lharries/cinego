82bf020d4420062bd3c0bd32594b38201b495679
lharries <luke.harries@me.com>
2017-12-02 10:28:51 +0000

    removed unneeded employee and customer login

--- src/models/Customer.java
+++ src/models/Customer.java
@@ -3,21 +3,6 @@ package models;
 import java.util.ArrayList;
 
 public class Customer extends User {
-    private ArrayList<Booking> bookings = new ArrayList<Booking>();
 
-    public Customer() {
-    }
-
-    public ArrayList<Booking> getBookings() {
-        return this.bookings;
-    }
-
-    public ArrayList<Booking> getUpcomingBookings() {
-        // TODO
-        return this.bookings;
-    }
-
-    public Booking createBooking() {
-        return new Booking();
-    }
+    public Customer() {}
 }
